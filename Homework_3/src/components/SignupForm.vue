3ea479a826f4c7c78b11ac3ddf0d6dd3aacb7430
karlerik-t <karlerik.toomemagi@gmail.com>
2023-11-28 03:33:49 +0200

    Password validation (task 6)

--- Homework_3/src/components/SignupForm.vue
+++ Homework_3/src/components/SignupForm.vue
@@ -1,53 +1,90 @@
 <template>
-    <div class="parent">
-      <div class="child">
-        <div>
-          <p>Welcome to PostIt!</p>
-        </div><br>
-        <div>
-          <p id="text">Create an account<br>or<br>Please log in</p>
-        </div><br>
-        <form action="index.html" method="post">
-          <div><input type="text" name="email" placeholder="Email" required>
-          </div><br>
-          <input type="text" name="password" placeholder="Password" required><br>
-          <div>
-            <button type="submit">Login</button>
-          </div>
-        </form>
-        <div><p id="text">Forget password</p></div>
+  <div class="parent">
+    <div class="child">
+      <div>
+        <p>Welcome to PostIt!</p>
       </div>
+      <br />
+      <div>
+        <p id="text">Create an account<br />or<br />Please log in</p>
+      </div>
+      <br />
+      <form @submit="validatePasswd">
+        <div>
+          <input type="text" name="email" placeholder="Email" required />
+        </div>
+        <br />
+        <input
+          type="password"
+          id="passwd"
+          name="password"
+          placeholder="Password"
+          title="Invalid password"
+          minlength="8"
+          maxlength="14"
+          required
+        /><br />
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      <div><p id="text">Forget password</p></div>
     </div>
-    </template>
+  </div>
+</template>
     
-    <script>
+<script>
+export default {
+  name: "SignupForm",
+
+  methods: {
+    validatePasswd: function (e) {
+      var passwd = document.getElementById("passwd").value;
+
+      var regexNumber = /\d+/.test(passwd);
+      // Since the first letter has to be uppercase, there is no need for an additional uppercase character check
+      var regexFirstUppercase = /^[A-Z]/.test(passwd);
+      var regexTwoLowercase = /^(?=.*[a-z].*[a-z]).*$/.test(passwd);
+      var regexUnderscore = /_/g.test(passwd);
+
+      if (
+        regexNumber &&
+        regexFirstUppercase &&
+        regexTwoLowercase &&
+        regexUnderscore
+      ) {
+        this.$router.push("/");
+      } else {
+        alert(
+          "Invalid password. Make sure that the following conditions are fulfilled:\n\n•Two lowercase characters\n•One numeric value\n•First character is an uppercase letter\n•Includes character '_'"
+        );
+        e.preventDefault();
+      }
+    },
+  },
+};
+</script>
     
-    export default {
-      name: 'SignupForm'
-    }
-    </script>
-    
-    <style>
-      .parent {
-      display: flex;
-      margin: auto;
-      width: 300px;
-      height: 400px;
-      background: #d8d8d8;
-      justify-content: center;
-      border-radius: 25px;
-    }
-    
-    .child {
-      background-color: rgba(148, 186, 230, 0);
-      width: 250px;
-      height: 300px;
-      margin-top: 50px;
-      text-align: center;
-      justify-content: center;
-      font-size: medium;
-      overflow: hidden;
-      font-family: Tahoma;
-    }
-    </style>
-    
+<style>
+.parent {
+  display: flex;
+  margin: auto;
+  width: 300px;
+  height: 400px;
+  background: #d8d8d8;
+  justify-content: center;
+  border-radius: 25px;
+}
+
+.child {
+  background-color: rgba(148, 186, 230, 0);
+  width: 250px;
+  height: 300px;
+  margin-top: 50px;
+  text-align: center;
+  justify-content: center;
+  font-size: medium;
+  overflow: hidden;
+  font-family: Tahoma;
+}
+</style>
